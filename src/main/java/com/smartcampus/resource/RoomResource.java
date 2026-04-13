64d492a9dcb89f0123f5636c35d7f63d7ab53fa2
Ananth-Abi <abiananth50@gmail.com>
2026-04-13 15:32:16 +0530

    Add RoomResource and SensorResource endpoints

--- src/main/java/com/smartcampus/resource/RoomResource.java
+++ src/main/java/com/smartcampus/resource/RoomResource.java
@@ -5,7 +5,13 @@ import com.smartcampus.exception.DataNotFoundException;
 import com.smartcampus.exception.RoomNotEmptyException;
 import com.smartcampus.model.Room;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
--- src/main/java/com/smartcampus/resource/SensorResource.java
+++ src/main/java/com/smartcampus/resource/SensorResource.java
@@ -6,7 +6,14 @@ import com.smartcampus.exception.LinkedResourceNotFoundException;
 import com.smartcampus.model.Room;
 import com.smartcampus.model.Sensor;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
